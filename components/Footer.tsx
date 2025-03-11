96f632e2222d742aba49eb107a2eed7ae8d6ec80
avishekshrestha <avishekshrestha57@gmail.com>
2025-03-11 14:31:16 +0545

    Resolved merge conflicts in package.json and globals.css

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <footer className="py-8 bg-gray-800 text-white text-center">
+        <p>&copy; 2025 TroposFly. All rights reserved.</p>
+        <p>Terms | Privacy Policy</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
